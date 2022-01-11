2968bdb0031d23dfdedf35cface74971739649d0
A. M. Shehab Khan <shehabkhan013@gmail.com>
2022-01-11 15:20:00 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,6 @@
 <?php
 
 
-<?php
-
-
 require get_template_directory() .'/inc/plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://plugaddons.com/json/composer.json',
