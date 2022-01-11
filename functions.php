6c0e7040071935bf0fa6ff0f53d309518633b37d
A. M. Shehab Khan <shehabkhan013@gmail.com>
2022-01-11 15:19:47 +0600

    add

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
 
 
-require 'path/to/plugin-update-checker/plugin-update-checker.php';
+<?php
+
+
+require get_template_directory() .'/inc/plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'http://example.com/path/to/details.json',
+	'https://plugaddons.com/json/composer.json',
 	__FILE__, //Full path to the main plugin file or functions.php.
 	'unique-plugin-or-theme-slug'
 );
